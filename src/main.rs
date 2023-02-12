68888d0e804ea718b776239bdc2365818feb85bb
guilhermegcaires <guilhermegcaires@gmail.com>
2023-02-11 21:00:23 -0300

    Pronto

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,23 @@
 use std::collections::HashMap;
-use std::fs::File;
-use std::io::{BufRead, BufReader};
+use std::fs::{File, self};
 
 fn main() {
-    let file = File::open("./tratado.csv").unwrap();
-    let reader = BufReader::new(file);
     let mut table: HashMap<String, i32> = HashMap::new();
+    let file = fs::read_to_string("./tratado.csv").unwrap();
     let mut total = 0;
+    let mut i = 0;
 
-    for lin in reader.lines() {
-        let line = lin.unwrap();
-        let word = line.split(",").nth(4).unwrap();
+    while i <= 100 {
+        for lin in file.lines() {
+            let line = lin;
+            let word = line.split(",").nth(4).unwrap();
 
-        let mut count = table.entry(word.to_string()).or_insert(0);
-        *count += 1;
-        total += 1;
+            let mut count = table.entry(word.to_string()).or_insert(0);
+            *count += 1;
+            total += 1;
+        }
+        i += 1;
     }
-
 
     println!("{:?}", total);
     println!("{:?}", table);
